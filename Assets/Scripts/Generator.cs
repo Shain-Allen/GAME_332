f9796f875a9fee5572f09247c2f28d6184370eec
Shain-Allen <48696021+Shain-Allen@users.noreply.github.com>
2024-04-18 16:05:28 -0700

    Fixed Generator and KeyManager scripts

--- Assets/Scripts/Generator.cs
+++ Assets/Scripts/Generator.cs
@@ -1,30 +1,28 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Generator : MonoBehaviour
 {
-    GameObject L;
-    GameObject FPSController;
-    public bool haveKeyL;
+    private MeshRenderer GeneratorKeyMesh;
+    KeyManager FPSController;
+    public Keys key;
     public string facingCheck;
 
     // Start is called before the first frame update
     void Start()
     {
-        L = GameObject.Find("Generator_L_Key"); //Find the game object labeled "Generator_L_Key"
-        L.GetComponent<MeshRenderer>().enabled = false; //Start with "L" object invisible
-        FPSController = GameObject.Find("FPSController"); //Find the game object labeled "FPSController"
+        GeneratorKeyMesh = transform.Find("Generator_Key").GetComponent<MeshRenderer>(); //Find the keymesh of generator
+        
+        GeneratorKeyMesh.enabled = false; //Start with Generator_Key object invisible
+        FPSController = FindFirstObjectByType<KeyManager>(); //Find the game object labeled "FPSController"
     }
 
     private void Update()
     {
-        facingCheck = FPSController.GetComponent<KeyManager>().imLookingAt; ; //Set facingCheck to imLookingAt from the KeyManager
-        haveKeyL = FPSController.GetComponent<KeyManager>().L_Key; //Set haveKeyL to L_Key from the KeyManager
+        facingCheck = FPSController.imLookingAt; ; //Set facingCheck to imLookingAt from the KeyManager
 
-        if (haveKeyL == true && facingCheck == "Generator_L" && Input.GetKeyDown("e")) //If you have the key, facing the generator, and hit "e" on the keyboard
+        if (FPSController.foundKeys.Contains(key) && gameObject.CompareTag(facingCheck) && Input.GetKeyDown("e")) //If you have the key, facing the generator, and hit "e" on the keyboard
         {
-            L.GetComponent<MeshRenderer>().enabled = true; //Enable the visibility for "L"
+            GeneratorKeyMesh.enabled = true; //Enable the visibility for "L"
         }
     }
 
--- Assets/Scripts/KeyManager.cs
+++ Assets/Scripts/KeyManager.cs
@@ -1,31 +1,37 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum Keys
+{   
+    L,
+    T,
+    Z
+}
+
 public class KeyManager : MonoBehaviour
 {
-    public bool L_Key;
+    public List<Keys> foundKeys = new List<Keys>(); //List of Keys>
 
     [SerializeField]
-    private GameObject mainCamera;
-    private float maxDistance;
+    private Camera mainCamera;
+    [SerializeField] private float maxDistance;
     public string imLookingAt;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        L_Key = false; //Start with the "L" shape hidden
+        foundKeys.Clear();
+        mainCamera = Camera.main;
     }
 
     void Update()
     {
         int x = Screen.width / 2; //Center along X
         int y = Screen.height / 2; //Center along Y
-        Ray ray = mainCamera.GetComponent<Camera>().ScreenPointToRay(new Vector3(x, y)); //Raycast from center of the camera
-        RaycastHit hit;
+        Ray ray = mainCamera.ScreenPointToRay(new Vector3(x, y)); //Raycast from center of the camera
 
-        if (Physics.Raycast(ray, out hit, maxDistance)) //If the raycast sees something within the max distance 
+        if (Physics.Raycast(ray, out RaycastHit hit, maxDistance)) //If the raycast sees something within the max distance 
         {
             imLookingAt = hit.collider.tag; //Set imLookingAt to the tag of the visible object
         }
@@ -34,9 +40,19 @@ public class KeyManager : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.tag == "L")
+        if (other.gameObject.CompareTag("L"))
         {
-            L_Key = true; //Set L_Key to true
+            foundKeys.Add(Keys.L); //Add L to the list of found keys
+            Destroy(other.gameObject); //Destroy the game object 
+        }
+        else if (other.gameObject.CompareTag("T"))
+        {
+            foundKeys.Add(Keys.T); //Add T to the list of found keys
+            Destroy(other.gameObject); //Destroy the game object 
+        }
+        else if (other.gameObject.CompareTag("Z"))
+        {
+            foundKeys.Add(Keys.Z); //Add Z to the list of found keys
             Destroy(other.gameObject); //Destroy the game object 
         }
     }
